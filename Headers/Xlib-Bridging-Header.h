058fd0a4c66ff5f114216de5a2b97b39109d2b4b
KCommit <103474404+KCommit@users.noreply.github.com>
2022-11-18 09:13:15 +0800

    add Xlib

---
+++ Headers/Xlib-Bridging-Header.h
@@ -0,0 +1,4 @@
+#ifndef XLIB_BRIDGING_HEADER_H
+#define XLIB_BRIDGING_HEADER_H
+#import <X11/Xlib.h>
+#endif
--- Sources/SwiftXlib/SwiftXlib.swift
+++
@@ -1,6 +0,0 @@
-public struct SwiftXlib {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
